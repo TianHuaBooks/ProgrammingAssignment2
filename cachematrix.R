bc95aa29cd91a3c339f2b15e9124964807a00c66
Wade Ju <wadeju@yahoo.com>
2014-08-22 22:26:18 -0700

    add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To provide cache function to get inverse matrix 
+## Need library MASS
 
-## Write a short comment describing this function
 
+## utility to make sure required library is installed
+pkgTest<-function(x) {
+	if (!require(x, character.only=TRUE))
+	{
+	    install.packages(x,dep=TRUE)
+	    if (!require(x, character.only=TRUE))
+		stop("Package not found!")
+	}
+}
+
+##  makeCacheMatrix to return a list of get/set/setinverse/getinverse
+##  function to manage inverse matrix cache
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	set<-function(y) {
+		x<<-y
+		m<<-NULL
+	}
+	get<-function() x
+	setinverse <- function(inverse) m<<-inverse
+	getinverse <- function() m
+	list(set=set, get = get, setinverse=setinverse, getinverset=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve is to return an inverse matrix by using makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	if (!is.null(i)) {
+		message("getting cached inverse matrix")
+		return (i)
+	}
+	## make sure MASS package is installed
+	pkgTest("MASS")
+	data<-x$get()
+	i <- ginv(data)
+	x$setinverse(i)
+	return (i)
 }
